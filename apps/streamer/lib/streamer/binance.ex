704228f3b40d77df174d91b68ac761fea3a1b087
SunLee128 <sunlee128@gmail.com>
2021-07-30 15:07:27 +1000

    Set streamer

--- apps/streamer/lib/streamer/binance.ex
+++ apps/streamer/lib/streamer/binance.ex
@@ -1,18 +1,45 @@
 defmodule Streamer.Binance do
   use WebSockex
-  @stream_endpoint ""
+  require Logger
 
-  def start_link(url, state) do
-    WebSockex.start_link(url, __MODULE__, state)
+  @stream_endpoint "wss://stream.binance.com:9443/ws/"
+
+  def start_link(symbol) do
+    symbol = String.downcase(symbol)
+
+    WebSockex.start_link(
+      "#{@stream_endpoint}#{symbol}@trade",
+      __MODULE__,
+      nil
+    )
   end
 
   def handle_frame({type, msg}, state) do
-    IO.puts "Received Message - Type: #{inspect type} -- Message: #{inspect msg}"
+    case Jason.decode(msg) do
+      {:ok, event} -> process_event(event)
+      {:error, _} -> Logger.error("Unable to parse msg: #{msg}")
+    end
+
     {:ok, state}
   end
 
-  def handle_cast({:send, {type, msg} = frame}, state) do
-    IO.puts "Sending #{type} frame with payload: #{msg}"
-    {:reply, frame, state}
+  defp process_event(%{"e" => "trade"} = event) do
+    trade_event = %Streamer.Binance.TradeEvent{
+      :event_type => event["e"],
+      :event_time => event["E"],
+      :symbol => event["s"],
+      :trade_id => event["t"],
+      :price => event["p"],
+      :quantity => event["q"],
+      :buyer_order_id => event["b"],
+      :seller_order_id => event["a"],
+      :trade_time => event["T"],
+      :buyer_market_maker => event["m"]
+    }
+
+    Logger.debug(
+      "Trade event received " <>
+        "#{trade_event.symbol}@#{trade_event.price}"
+    )
   end
 end
